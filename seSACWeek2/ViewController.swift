a5c0c316b14d671bea393d9103acd9c7575aedbd
Glsme <glasses.str.man@gmail.com>
2022-07-11 16:49:50 +0900

    Initial Commit

---
+++ seSACWeek2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seSACWeek2
+//
+//  Created by Seokjune Hong on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
